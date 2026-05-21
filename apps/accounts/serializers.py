86421c987088002a1598cc597d39b6552f52dae7
Babita Shrestha <shresthababita119@gmail.com>
2026-05-21 13:54:24 +0545

    image field correction

--- apps/accounts/serializers.py
+++ apps/accounts/serializers.py
@@ -1,19 +1,88 @@
-from rest_framework import serializers
+# from rest_framework import serializers
+# from django.contrib.auth.models import User
+# from .models import UserProfile
+
+
+# class UserProfileSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = UserProfile
+#         fields = ['full_name', 'phone', 'image']
+
+
+# class UserSerializer(serializers.ModelSerializer):
+
+#     full_name = serializers.CharField(source='profile.full_name', read_only=True)
+#     phone = serializers.CharField(source='profile.phone', read_only=True)
+#     image = serializers.ImageField(source='profile.image', read_only=True)
+
+#     class Meta:
+#         model = User
+#         fields = [
+#             'id',
+#             'username',
+#             'email',
+#             'full_name',
+#             'phone',
+#             'image',
+#         ]
+
+
+# class RegisterSerializer(serializers.ModelSerializer):
+
+#     password = serializers.CharField(write_only=True)
+#     full_name = serializers.CharField(write_only=True)
+#     phone = serializers.CharField(write_only=True)
+#     image = serializers.ImageField(write_only=True, required=False)
+
+#     class Meta:
+#         model = User
+#         fields = ['username', 'email', 'password', 'full_name', 'phone', 'image']
+
+#     def create(self, validated_data):
+
+#         full_name = validated_data.pop('full_name')
+#         phone = validated_data.pop('phone')
+#         image = validated_data.pop('image', None)
+
+#         user = User.objects.create_user(
+#             username=validated_data['username'],
+#             email=validated_data['email'],
+#             password=validated_data['password']
+#         )
+
+#         UserProfile.objects.create(
+#             user=user,
+#             full_name=full_name,
+#             phone=phone,
+#             image=image
+#         )
+
+#         return user
+
+
+
 from django.contrib.auth.models import User
+from rest_framework import serializers
 from .models import UserProfile
 
 
+# -------------------------
+# USER PROFILE SERIALIZER
+# -------------------------
 class UserProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserProfile
         fields = ['full_name', 'phone', 'image']
 
 
+# -------------------------
+# USER LIST SERIALIZER
+# -------------------------
 class UserSerializer(serializers.ModelSerializer):
 
-    full_name = serializers.CharField(source='profile.full_name', read_only=True)
-    phone = serializers.CharField(source='profile.phone', read_only=True)
-    image = serializers.ImageField(source='profile.image', read_only=True)
+    full_name = serializers.CharField(source='userprofile.full_name', read_only=True)
+    phone = serializers.CharField(source='userprofile.phone', read_only=True)
+    image = serializers.ImageField(source='userprofile.image', read_only=True)
 
     class Meta:
         model = User
@@ -27,19 +96,28 @@ class UserSerializer(serializers.ModelSerializer):
         ]
 
 
+# -------------------------
+# REGISTER SERIALIZER
+# -------------------------
 class RegisterSerializer(serializers.ModelSerializer):
 
     password = serializers.CharField(write_only=True)
     full_name = serializers.CharField(write_only=True)
     phone = serializers.CharField(write_only=True)
-    image = serializers.ImageField(write_only=True, required=False)
+    image = serializers.ImageField(required=False, allow_null=True)
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password', 'full_name', 'phone', 'image']
+        fields = [
+            'username',
+            'email',
+            'password',
+            'full_name',
+            'phone',
+            'image'
+        ]
 
     def create(self, validated_data):
-
         full_name = validated_data.pop('full_name')
         phone = validated_data.pop('phone')
         image = validated_data.pop('image', None)
--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,47 +1,41 @@
-from rest_framework.viewsets import ReadOnlyModelViewSet
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
+
+
+
+
+
 
 from django.contrib.auth.models import User
 
-from .serializers import UserSerializer, RegisterSerializer
+from rest_framework.viewsets import ReadOnlyModelViewSet
+from rest_framework.generics import CreateAPIView
+from rest_framework.parsers import MultiPartParser, FormParser
+from rest_framework.response import Response
+from rest_framework import status
+
 from drf_spectacular.utils import extend_schema
-from .serializers import RegisterSerializer
+
+from .serializers import UserSerializer, RegisterSerializer
+
 
 class UserViewSet(ReadOnlyModelViewSet):
-
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
 
-# class RegisterView(APIView):
 
-#     def post(self, request):
-#         serializer = RegisterSerializer(data=request.data)
+@extend_schema(
+    request={
+        "multipart/form-data": RegisterSerializer
+    },
+    responses={201: RegisterSerializer},
+    description="Register user with image upload"
+)
+class RegisterView(CreateAPIView):
+    serializer_class = RegisterSerializer
+    parser_classes = (MultiPartParser, FormParser)
 
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response({"message": "User created successfully"}, status=201)
-
-#         return Response(serializer.errors, status=400)
-
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
-from drf_spectacular.utils import extend_schema
-
-from .serializers import RegisterSerializer
-
-
-class RegisterView(APIView):
-
-    @extend_schema(
-        request=RegisterSerializer,
-        responses={201: RegisterSerializer}
-    )
-    def post(self, request):
-        serializer = RegisterSerializer(data=request.data)
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
 
         if serializer.is_valid():
             serializer.save()
